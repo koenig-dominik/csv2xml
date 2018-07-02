24e89ee1fe04b275d360f58822cce409000a3709
Dominik König <koenig-dominik@users.noreply.github.com>
2018-07-03 00:21:06 +0200

    First working version

--- src/converter.ts
+++ src/converter.ts
@@ -1,22 +1,169 @@
 import * as csvParser from 'csv-parse';
-import * as xmlBuilder from 'xmlbuilder';
 import * as xml2js from 'xml2js';
 import * as fs from 'fs';
+import * as util from 'util';
+
+const readFile = util.promisify(fs.readFile);
+const parseXml = util.promisify(xml2js.parseString);
+
+const templateAttributeIdentifier = 'csv2xmlRepeat';
+const containsTemplateExpressionRegex = /\${.*}/;
+
+interface ReplaceXmlLocation {
+    value: string,
+    index: number,
+    parent: object
+}
 
 export default class Converter {
 
-    private xmlParser = new xml2js.Parser();
+    private xmlRepeatTemplate;
+    private xmlRepeatEntryPoint;
+    private xmlTemplate;
+    private xmlReplaceLocations: ReplaceXmlLocation[];
+    private xmlBuilder = new xml2js.Builder();
 
-    constructor(private delimiter: string, private xmlTemplatePath: string) {
-        fs.readFile(__dirname + xmlTemplatePath, (err, data) => {
-            this.xmlParser.parseString(data, (err, result) => {
-                console.log(result);
-            });
-        });
+    constructor(private delimiter: string, private xmlTemplatePath: string, private csvInputPath: string, private outputPath: string) {}
+
+    public async init() {
+        try {
+            const xmlTemplateCode = await readFile(this.xmlTemplatePath);
+            this.xmlTemplate = await parseXml(xmlTemplateCode);
+
+            const [xmlRepeatNode, xmlRepeatNodeEntry] = this.getXMLRepeatNodeAndEntry(this.xmlTemplate);
+            this.xmlRepeatTemplate = JSON.parse(JSON.stringify(xmlRepeatNode));
+
+            (xmlRepeatNodeEntry as any[]).length = 0; // This clears the array
+            this.xmlRepeatEntryPoint = xmlRepeatNodeEntry;
+
+            this.xmlReplaceLocations = Converter.getTemplateReplaceLocations(this.xmlRepeatTemplate);
+
+        } catch(error) {
+            if(error instanceof Error) {
+                error.message = 'Could not initialize converter: ' + error.message;
+            }
+            throw error;
+        }
     }
 
-    public run() {
-        //csvParser.
+    private getXMLRepeatNodeAndEntry(xml): [Object, Object] | null {
+        const rootNode = xml[Object.keys(xml)[0]];
+
+        const getXMLRepeatNodeRecursive = (nodeList): [Object, Object] | null => {
+            for(const node of nodeList) {
+                for(let [nodeName, childNodeList] of Object.entries(node)) {
+                    if(nodeName === '$') {
+                        if(childNodeList[templateAttributeIdentifier] !== undefined) {
+                            return [node, nodeList];
+                        }
+                        continue;
+                    }
+
+                    if(!(childNodeList instanceof Array)) {
+                        continue;
+                    }
+
+                    const result = getXMLRepeatNodeRecursive(childNodeList);
+                    if (result !== null) {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        };
+
+        for(const childNode of Object.values(rootNode)) {
+            const result = getXMLRepeatNodeRecursive(childNode);
+            if(result !== null) {
+                delete result[0]['$'][templateAttributeIdentifier];
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+    private static getTemplateReplaceLocations(template: object): ReplaceXmlLocation[] {
+        let replaceLocations = [];
+
+        const getTemplateReplaceLocationsRecurse = (node: object | string | string[] | object[], parent: object, index: number | string) => {
+            if(typeof node === 'string') { // The node is a direct value
+                if(node.match(containsTemplateExpressionRegex) !== null) { // Check if the value contains ${}
+                    replaceLocations.push({
+                        value: node,
+                        index: index,
+                        parent: parent
+                    })
+                }
+            } else if(node instanceof Array) { // The node is not a value and can have children
+                for(let i = 0; i < node.length; i++) {
+                    getTemplateReplaceLocationsRecurse(node[i], node, i);
+                }
+            } else { // The node must be an object
+                for(const [key, child] of Object.entries(node)) {
+                    getTemplateReplaceLocationsRecurse(child, node, key);
+                }
+            }
+        };
+
+        getTemplateReplaceLocationsRecurse(template, null, null);
+
+        return replaceLocations;
+    }
+
+    public async run() {
+        let csv = await this.getCsvData();
+        let records = Converter.getRecordMap(csv);
+
+        for(const record of records) {
+            this.xmlRepeatEntryPoint.push(this.getEvaluatedXmlNode(record));
+        }
+
+        let result = this.xmlBuilder.buildObject(this.xmlTemplate);
+
+        fs.writeFileSync(this.outputPath, result);
+    }
+
+    private getEvaluatedXmlNode(row: {[key: string]: string}): object {
+        for(let replaceLocation of this.xmlReplaceLocations) {
+            replaceLocation.parent[replaceLocation.index] = eval('`' + replaceLocation.value + '`');
+        }
+
+        return JSON.parse(JSON.stringify(this.xmlRepeatTemplate));
+    }
+
+    private static getRecordMap(data: string[][]): {[key: string]: string}[] {
+        let records: {}[] = [];
+
+        let header: string[];
+        for(let row of data) {
+            if(header === undefined) {
+                header = row;
+                continue;
+            }
+
+            const record = new Map<string, string>();
+            for(let i = 0; i < header.length; i++) {
+                record[header[i]] = row[i];
+            }
+
+            records.push(record);
+        }
+
+        return records;
+    }
+
+    private getCsvData(): Promise<string[][]> {
+        return new Promise((resolve, reject) => {
+            fs.createReadStream(this.csvInputPath).pipe(csvParser({delimiter: this.delimiter}, (err, data) => {
+                if(err) {
+                    reject(err);
+                }
+
+                resolve(data);
+            }));
+        })
     }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as meow from 'meow';
 import * as updateNotifier from 'update-notifier';
 import * as fs from 'fs';
+import * as path from 'path';
 import Converter from './converter';
 
 const cli = meow(`
@@ -8,10 +9,10 @@ const cli = meow(`
         $ csv2xml source destination -xt
 
     Options
-        -xt, --xmlTemplate   The XML template filepath
-        -s, --split          Split the output xml into multiple files, the destination path will be a folder
-        -vd, --validateDate  Validates dates using the specified format
-        -d, --delimiter      CSV delimiter
+        --xt, --xmlTemplate   The XML template file path
+        --s, --split          Split the output xml into multiple files, the destination path will be a folder
+        --vd, --validateDate  Validates dates using the specified format
+        --d, --delimiter      CSV delimiter
     Example
         $ csv2xml source/file destination/file
 `, {
@@ -38,8 +39,6 @@ const cli = meow(`
 
 updateNotifier(cli).notify();
 
-console.log(cli);
-
 if (cli.input.length < 2) {
     console.error('Specify source and destination path');
     process.exit(1);
@@ -49,9 +48,9 @@ if(cli.flags.xmlTemplate === undefined) {
     console.error('Specify the xml template path');
     process.exit(1);
 }
-cli.flags.xmlTemplate = __dirname + cli.flags.xmlTemplate;
+cli.flags.xmlTemplate = path.join(process.cwd(), cli.flags.xmlTemplate);
 if (!fs.existsSync(cli.flags.xmlTemplate)) {
-    console.error('The specified xml template does not exist');
+    console.error(`The specified xml template does not exist (${cli.flags.xmlTemplate})`);
     process.exit(1);
 }
 
@@ -59,11 +58,26 @@ if(cli.input[0] === undefined) {
     console.error('Specify the input csv file');
     process.exit(1);
 }
-cli.input[0] = __dirname + cli.input[0];
+
+cli.input[0] = path.join(process.cwd(), cli.input[0]);
 if (!fs.existsSync(cli.input[0])) {
-    console.error('The specified input csv file does not exist');
+    console.error(`The specified input csv file does not exist (${cli.input[0]})`);
     process.exit(1);
 }
 
-const converter = new Converter(cli.flags.delimiter, cli.flags.xmlTemplate);
-converter.run();
+if(cli.input[1] === undefined) {
+    console.error('Specify the output path');
+    process.exit(1);
+}
+
+cli.input[1] = path.join(process.cwd(), cli.input[1]);
+if (!fs.accessSync(cli.input[1])) {
+    console.error(`The specified output path cannot be written to (${cli.input[1]})`);
+    process.exit(1);
+}
+
+(async () => {
+    const converter = new Converter(cli.flags.delimiter, cli.flags.xmlTemplate, cli.input[0], cli.input[1]);
+    await converter.init();
+    await converter.run();
+})();
